fcf635acf2111e30f29edffa1bbe172bb187861f
Freaky_Lord <64467666+LordFreaky@users.noreply.github.com>
2026-06-29 20:31:52 +0200

    Update data.lua

--- data.lua
+++ data.lua
@@ -1,1 +1,25 @@
---data.lua
+data:extend({
+  {
+    type = "technology",
+    name = "heliopause-foundry-start-equipment",
+    icon = "__base__/graphics/technology/construction-robotics.png",
+    icon_size = 256,
+
+    enabled = false,
+    visible_when_disabled = true,
+
+    prerequisites = {"automation"},
+
+    effects = {},
+
+    unit = {
+      count = 25,
+      ingredients = {
+        {"automation-science-pack", 1}
+      },
+      time = 10
+    },
+
+    order = "a-b-a"
+  }
+})
