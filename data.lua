fa5bc1a1334a106f613d7fb3b4efefdfc81d8532
Freaky_Lord <64467666+LordFreaky@users.noreply.github.com>
2026-06-29 20:38:23 +0200

    Update data.lua

--- data.lua
+++ data.lua
@@ -1,17 +1,15 @@
+local signal_prerequisite = "rocket-silo"
+
 data:extend({
   {
     type = "technology",
     name = "heliopause-foundry-start-equipment",
     icon = "__base__/graphics/technology/construction-robotics.png",
     icon_size = 256,
-
     enabled = false,
     visible_when_disabled = true,
-
     prerequisites = {"automation"},
-
     effects = {},
-
     unit = {
       count = 25,
       ingredients = {
@@ -19,7 +17,28 @@ data:extend({
       },
       time = 10
     },
-
     order = "a-b-a"
+  },
+  {
+    type = "technology",
+    name = "heliopause-foundry-signal-from-space",
+    icon = "__base__/graphics/technology/rocket-silo.png",
+    icon_size = 256,
+    enabled = false,
+    visible_when_disabled = false,
+    prerequisites = {signal_prerequisite},
+    effects = {},
+    unit = {
+      count = 500,
+      ingredients = {
+        {"automation-science-pack", 1},
+        {"logistic-science-pack", 1},
+        {"chemical-science-pack", 1},
+        {"production-science-pack", 1},
+        {"utility-science-pack", 1}
+      },
+      time = 30
+    },
+    order = "z-h-f-a"
   }
 })
